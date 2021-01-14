d26d8cf951a80d1647e50e37ad8787190d7c9c21
rositatisor <70884246+rositatisor@users.noreply.github.com>
2021-01-14 21:03:46 +0200

    bug fixes

--- agurkai-oop-js/list-pea.php
+++
@@ -1,8 +0,0 @@
-<?php foreach ($store->getAll() as $darzove): ?>
-    <div class="items pea">
-        <img src="./img/pea-<?= $darzove->imgPath?>.jpg" alt="Agurko nuotrauka">
-        <p>Žirnis nr. <?= $darzove->id ?></p>
-        <p>Kiekis: <?= $darzove->kiekis ?></p>
-        <button class="rauti" type="button" name="rauti-zirni" value="<?= $darzove->id ?>">+</button>
-    </div>
-<?php endforeach ?>
